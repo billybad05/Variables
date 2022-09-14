fadb10495dcafb9326737bc1d9ef0998a0222f55
Billy Dotson <billybad5@gmail.com>
2022-09-14 16:13:52 -0400

    Add project files.

---
+++ Giraffe/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cn = "Tom";
+            int ca;
+            ca = 25;
+
+            Console.WriteLine($"There once was a man named {cn}");
+            Console.WriteLine("He was " +ca+ " years old");
+
+            cn = "Mike";
+
+            Console.WriteLine("He really liked the name " + cn);
+            Console.WriteLine($"But didnt like being {ca}");
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
